6e83433310f1c01619732889578aa56939f95f58
1990Company <admin@1990.company>
2023-02-04 15:45:20 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [sitemap()]
+});
